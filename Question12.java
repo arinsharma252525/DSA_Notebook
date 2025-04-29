6eb6703903933fdf9fb19130de27cabe6d0cc6ef
Arin Sharma <arinsharma252525@gmail.com>
2025-04-29 10:29:36 +0530

    day-3

--- Question12.java
+++ Question12.java
@@ -2,7 +2,7 @@
 public class Question12 {
     public static void bubbleSort(int arr[]) {
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] < arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
