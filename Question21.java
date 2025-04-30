c4178f7fae1459f9f90bf3593a28f8d715b77428
Arin Sharma <arinsharma252525@gmail.com>
2025-04-30 13:06:15 +0530

    day-4

--- Question21.java
+++ Question21.java
@@ -1,4 +1,4 @@
-//Write a program to Find Transpose of a Matrix.
+//Write a program to Find Transpose of a Matrix using for loops.
 public class Question21 {
     public static int[][] transposeMatrix(int[][] matrix) {
         int rows = matrix.length;
@@ -12,28 +12,24 @@ public class Question21 {
         return transpose;
     }
 
-    public static void printMatrix(int[][] matrix) {
-        System.out.println("The matrix is: ");
+
+    public static void main(String[] args) {
+        int[][] matrix = { { 2, 3, 7 }, { 5, 6, 7 } };
+        int[][] transposedMatrix = transposeMatrix(matrix);
+        System.out.println("Original Matrix:");
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
-    }
 
-    public static void main(String[] args) {
-        int[][] matrix = { { 2, 3, 7 }, { 5, 6, 7 } };
-
-        // Display original matrix
-        System.out.println("Original Matrix:");
-        printMatrix(matrix);
-
-        // Transpose the matrix
-        int[][] transpose = transposeMatrix(matrix);
-
-        // Print the transposed matrix
-        System.out.println("\nTransposed Matrix:");
-        printMatrix(transpose);
+        System.out.println("Transpose of the Matrix:");
+        for (int i = 0; i < transposedMatrix.length; i++) {
+            for (int j = 0; j < transposedMatrix[0].length; j++) {
+                System.out.print(transposedMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
