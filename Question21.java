b4cbc4ae381fb8f513b1812a435064d656244f65
Arin Sharma <arinsharma252525@gmail.com>
2025-04-30 13:12:11 +0530

    day-4

--- Question21.java
+++ Question21.java
@@ -1,11 +1,11 @@
 //Write a program to Find Transpose of a Matrix using for loops.
 public class Question21 {
     public static int[][] transposeMatrix(int[][] matrix) {
-        int rows = matrix.length;
-        int columns = matrix[0].length;
-        int[][] transpose = new int[columns][rows];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
+        int row = matrix.length;
+        int column = matrix[0].length;
+        int transpose [][] = new int[column][row];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
                 transpose[j][i] = matrix[i][j];
             }
         }
