851f77e29b2d9afd46e63b4530ffaf12a3a9d1c9
Arin Sharma <arinsharma252525@gmail.com>
2025-05-02 10:37:42 +0530

    day-5

--- Question27.java
+++ Question27.java
@@ -4,7 +4,7 @@ public class Question27 {
     public static int Vowels() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
-        String str = sc.nextLine(); // Use nextLine() to read the entire line
+        String str = sc.nextLine();
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
