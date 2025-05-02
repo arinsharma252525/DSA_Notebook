713e4b87bdb7d43ebf17f9aa5a8fc8beeb3b68c8
Arin Sharma <arinsharma252525@gmail.com>
2025-05-02 09:50:50 +0530

    day-5

--- Question27.java
+++ Question27.java
@@ -1,10 +1,10 @@
 //Count how many times lowercase vowels occurred in a String entered by the user
 import java.util.Scanner;
 public class Question27 {
-    public static void Vowels() {
+    public static int Vowels() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
-        String str = sc.next();
+        String str = sc.nextLine(); // Use nextLine() to read the entire line
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
@@ -12,10 +12,10 @@ public class Question27 {
                 count++;
             }
         }
-        System.out.println("vowels: " + count);
+        return count;
     }
 
     public static void main(String[] args) {
-        Vowels();
+        System.out.println(Vowels());
     }
 }
--- Question28.java
+++ Question28.java
@@ -2,10 +2,11 @@
 public class Question28 {
   public static boolean checkAnagrams(String str1, String str2) {
 
-    if (str1.length() != str2.length())
+    if (str1.length() != str2.length()) {
       return false;
+    }
 
-    int[] freq = new int[26];
+    int freq[] = new int[26];
     for (int i = 0; i < str1.length(); i++) {
       freq[str1.charAt(i) - 'a']++;
     }
@@ -20,8 +21,8 @@ public class Question28 {
   }
 
   public static void main(String args[]) {
-    String Str1 = "geeks";
-    String Str2 = "kseeg";
+    String Str1 = "arin";
+    String Str2 = "rain";
     System.out.println(checkAnagrams(Str1, Str2));
   }
 }
