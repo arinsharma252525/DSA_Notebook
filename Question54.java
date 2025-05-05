3c790f714896c283718d5915e2ead0fe5ec96be1
Arin Sharma <arinsharma252525@gmail.com>
2025-05-05 13:24:51 +0530

    DSA Question

--- Question54.java
+++ Question54.java
@@ -1,5 +1,21 @@
+//Remove Duplicates
 public class Question54 {
+    public static void removeDuplicates(String str, int index, StringBuilder newstr, boolean map[]) {
+        if (index == str.length()) {
+            System.out.println(newstr);
+            return;
+        }
+        char currChar = str.charAt(index);
+        if (map[currChar - 'a'] == true) {
+            removeDuplicates(str, index + 1, newstr, map);
+        } else {
+            map[currChar - 'a'] = true;
+            removeDuplicates(str, index + 1, newstr.append(currChar), map);
+        }
+    }
+
     public static void main(String[] args) {
-        
+    String str = "arinsharma";
+    removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
     }
 }
