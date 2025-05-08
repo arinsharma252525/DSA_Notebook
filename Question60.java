401316e9104b1c967db1504b671dd03b90a18566
Arin Sharma <arinsharma252525@gmail.com>
2025-05-08 18:53:48 +0530

    DSA Question

--- Question60.java
+++ Question60.java
@@ -8,9 +8,8 @@ public class Question60 {
             return 0;
         }
 
-        int res = countSubstrs(str, i + 1, j, n - 1) +
-                countSubstrs(str, i, j - 1, n - 1) -
-                countSubstrs(str, i + 1, j - 1, n - 2);
+        int res = countSubstrs(str, i + 1, j, n - 1) + countSubstrs(str, i, j - 1, n - 1)
+                - countSubstrs(str, i + 1, j - 1, n - 2);
 
         if (str.charAt(i) == str.charAt(j)) {
             res++;
