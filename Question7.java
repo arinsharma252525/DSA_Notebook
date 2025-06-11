634f462fba6c86383a4a45be3d379215245ed93a
Arin Sharma <arinsharma252525@gmail.com>
2025-06-11 16:22:10 +0530

    DSA-Question

--- Question7.java
+++ Question7.java
@@ -2,14 +2,14 @@
 public class Question7 {
     public static int maxSubarraySum(int arr[]) {
         int maxSum = Integer.MIN_VALUE;
-        int currrSum = 0;
+        int currSum = 0;
         for (int i = 0; i < arr.length; i++) {
-            currrSum += arr[i];
-            if (currrSum > maxSum) {
-                maxSum = currrSum;
+            currSum += arr[i];
+            if (currSum > maxSum) {
+                maxSum = currSum;
             }
-            if (currrSum < 0) {
-                currrSum = 0;
+            if (currSum < 0) {
+                currSum = 0;
             }
         }
         return maxSum;
