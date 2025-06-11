9266a996d72a6c1847152c5cd3402956f8ed9cd5
Arin Sharma <arinsharma252525@gmail.com>
2025-06-11 14:12:41 +0530

    DSA-Question

--- Question9.java
+++ Question9.java
@@ -1,18 +1,15 @@
 //Best Time to Buy and Sell Stock
 public class Question9 {
     public static int maxProfit(int[] prices) {
-        int buyPrice = Integer.MAX_VALUE;
         int max = 0;
-        for (int i = 0; i < prices.length; i++) {
-            if (buyPrice < prices[i]) {
-                int profit = prices[i] - buyPrice;
-                max = Math.max(max, profit);
-            } else {
-                buyPrice = prices[i];
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                max = Math.max(max, prices[j] - prices[i]);
             }
         }
         return max;
-    } 
+    }
+
     public static void main(String args[]) {
         int prices[] = {7, 10, 1, 3, 6, 9, 2};
         System.out.println(maxProfit(prices));
