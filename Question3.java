acf4c3c3dd87abb720a4d73813783060d33d5838
Arin Sharma <arinsharma252525@gmail.com>
2025-06-20 09:18:25 +0530

    DSA-Question

--- Question3.java
+++ Question3.java
@@ -3,22 +3,19 @@ public class Question3 {
     public static int binarySearch(int arr[], int target) {
         int start = 0;
         int end = arr.length - 1;
-        int result = -1;
-
         while (start <= end) {
-            int mid = start + (end - start) / 2;
-
+            int mid = (start + end) / 2;
             if (arr[mid] == target) {
-                result = mid;
-                end = mid - 1;
-            } else if (arr[mid] < target) {
+                return mid;
+            }
+            if (arr[mid] < target) {
                 start = mid + 1;
             } else {
                 end = mid - 1;
             }
-        }
-        return result;
-    }
+        } 
+        return -1;
+    } 
 
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, 4, 5 };
