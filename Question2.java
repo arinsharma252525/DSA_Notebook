a09a2236a74c3fe64f819393f57b37a0b5812194
Arin Sharma <arinsharma252525@gmail.com>
2025-07-22 10:49:06 +0530

    Basic-Sorting-Questions

---
+++ Question2.java
@@ -0,0 +1,24 @@
+//Selection Sort
+public class Question2 {
+    public static void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minIndex] > arr[j]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 6, 3, 8, 1, 9, 2, 4, 5, 7 };
+        selectionSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
